84de7d8eb798392104df0e62289297cc9c5b8a34
Goolius <julius.franklin@student.uts.edu.au>
2026-09-13 17:52:31 +1000

    Initial Commit

---
+++ MediLog/MediLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediLogApp.swift
+//  MediLog
+//
+//  Created by My Mac on 13/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct MediLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
